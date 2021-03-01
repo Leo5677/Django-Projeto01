d1475de8bcfba6cdd04d18e0dd00e19316dc7290
Leo5677 <48198740+Leo5677@users.noreply.github.com>
2021-02-28 21:50:47 -0300

    Projetofinalizado

--- django1/settings.py
+++ django1/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#n#rc8%d+&wa3mtuj1xw@$9-r=@do2ypmaj51id(rs^w08e9=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
